3b24a65e6caebac9e6e74004de7eff70cb34b106
pantherr99 <bulamayacaksin99@gmail.com>
2026-04-06 02:49:48 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { 
+    getDatabase, 
+    ref, 
+    set, 
+    get, 
+    child, 
+    remove, 
+    onValue  // <--- BURAYA EKLE
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjMAo3sazNWx5aNFS9RnOahAqNOxyMmjA",
-  authDomain: "panter-f9c0e.firebaseapp.com",
-  projectId: "panter-f9c0e",
-  storageBucket: "panter-f9c0e.firebasestorage.app",
-  messagingSenderId: "123104603113",
-  appId: "1:123104603113:web:88d6eb21f9f27295e1f978",
-  // Senin resimdeki tam link buraya:
-  databaseURL: "https://panter-f9c0e-default-rtdb.europe-west1.firebasedatabase.app" 
+    // Senin kendi config bilgilerin burada kalsın
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, set, get, child };
+// Hepsini export ettiğinden emin ol
+export { db, ref, set, get, child, remove, onValue };
